48a213cac19591323d5936cddadc43525cb32a7d
salasin <slava1413@gmail.com>
2022-05-18 16:01:52 +0300

    Upload recent changes to HW2.

--- HW2/deploy.sh
+++ HW2/deploy.sh
@@ -1,86 +1,147 @@
-# Copied from https://gist.github.com/ayende/db14de14bcd4e0603eb30f26914d9a2b
+# Branched from https://gist.github.com/ayende/db14de14bcd4e0603eb30f26914d9a2b
 
-KEY_NAME="salasin-cloudcomputing-hw2"
+KEY_NAME="key-`date +'%N'`"
 KEY_PEM="$KEY_NAME.pem"
+UBUNTU_20_04_AMI="ami-042e8287309f5df03"
+REDIS_SERVING_PORT=6379
+ENDPOINT_SERVING_PORT=5000
+MY_IP=$(curl ipinfo.io/ip)
 
 echo "create key pair $KEY_PEM to connect to instances and save locally"
 aws ec2 create-key-pair --key-name $KEY_NAME \
     | jq -r ".KeyMaterial" > $KEY_PEM
-
 # secure the key pair
 chmod 400 $KEY_PEM
 
-SEC_GRP="my-sg-`date +'%N'`"
+########################################################################################################################
+#
+# SETUP OF REDIS SERVER
+#
+########################################################################################################################
 
-echo "setup firewall $SEC_GRP"
+REDIS_SEC_GRP="redis-sg-`date +'%N'`"
+
+echo "setup security group for Redis server..."
 aws ec2 create-security-group   \
-    --group-name $SEC_GRP       \
-    --description "Access my instances"
+    --group-name $REDIS_SEC_GRP       \
+    --description "Redis server security group"
 
-# figure out my ip
-MY_IP=$(curl ipinfo.io/ip)
-echo "My IP: $MY_IP"
-
-
-echo "setup rule allowing SSH access to $MY_IP only"
+echo "setup rule allowing SSH access to Redis server..."
 aws ec2 authorize-security-group-ingress        \
-    --group-name $SEC_GRP --port 22 --protocol tcp \
+    --group-name $REDIS_SEC_GRP --port 22 --protocol tcp \
     --cidr $MY_IP/32
 
-echo "setup rule allowing HTTP (port 6379) access to $MY_IP only"
+echo "setup rule allowing HTTP (port $REDIS_SERVING_PORT) access to Redis server..."
 aws ec2 authorize-security-group-ingress        \
-    --group-name $SEC_GRP --port 6379 --protocol tcp \
-    --cidr $MY_IP/32
+    --group-name $REDIS_SEC_GRP --port $REDIS_SERVING_PORT --protocol tcp \
+    --cidr 0.0.0.0/0
 
-UBUNTU_20_04_AMI="ami-042e8287309f5df03"
-
-echo "Creating Ubuntu 20.04 instance..."
-RUN_INSTANCES=$(aws ec2 run-instances   \
+echo "creating Redis server..."
+RUN_REDIS_SERVER=$(aws ec2 run-instances   \
     --image-id $UBUNTU_20_04_AMI        \
     --instance-type t3.micro            \
     --key-name $KEY_NAME                \
-    --security-groups $SEC_GRP)
+    --security-groups $REDIS_SEC_GRP)
 
-INSTANCE_ID=$(echo $RUN_INSTANCES | jq -r '.Instances[0].InstanceId')
+REDIS_SERVER_INSTANCE_ID=$(echo $RUN_REDIS_SERVER | jq -r '.Instances[0].InstanceId')
 
-echo "Waiting for instance creation..."
-aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+aws ec2 wait instance-running --instance-ids $REDIS_SERVER_INSTANCE_ID
 
-PUBLIC_IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID |
+REDIS_SERVER_IP=$(aws ec2 describe-instances  --instance-ids $REDIS_SERVER_INSTANCE_ID |
     jq -r '.Reservations[0].Instances[0].PublicIpAddress'
 )
 
-echo "New instance $INSTANCE_ID @ $PUBLIC_IP"
+echo "Redis server $REDIS_SERVER_INSTANCE_ID @ $REDIS_SERVER_IP"
 
-echo "setup production environment"
-ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
-    curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+echo "setup Redis server..."
+ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$REDIS_SERVER_IP <<EOF
     sudo apt-get update
-    sudo apt-get install redis
-    # cleaning up port 6379
+    yes Y | sudo apt-get install redis-server
     sudo service redis-server stop
-    redis-server --port 6379 --daemonize yes --protected-mode no
+    redis-server --port $REDIS_SERVING_PORT --daemonize yes --protected-mode no
     exit
 EOF
 
-# setup the endpoints
-ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
-    curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+########################################################################################################################
+#
+# SETUP OF ENDPOINT SERVERS
+#
+########################################################################################################################
+
+ENDPOINT_SERVER_SEC_GRP="endpoint-server-sg-`date +'%N'`"
+
+echo "setup security group for endpoint server..."
+aws ec2 create-security-group   \
+    --group-name $ENDPOINT_SERVER_SEC_GRP      \
+    --description "Endpoint server security group"
+
+echo "setup rule allowing SSH access to endpoint servers"
+aws ec2 authorize-security-group-ingress        \
+    --group-name $ENDPOINT_SERVER_SEC_GRP --port 22 --protocol tcp \
+    --cidr $MY_IP/32
+
+echo "setup rule allowing HTTP (port $ENDPOINT_SERVING_PORT) access to endpoint server"
+aws ec2 authorize-security-group-ingress        \
+    --group-name $ENDPOINT_SERVER_SEC_GRP --port $ENDPOINT_SERVING_PORT --protocol tcp \
+    --cidr 0.0.0.0/0
+
+echo "creating primary endpoint server..."
+RUN_PRIMARY_ENDPOINT_SERVER=$(aws ec2 run-instances   \
+    --image-id $UBUNTU_20_04_AMI        \
+    --instance-type t3.micro            \
+    --key-name $KEY_NAME                \
+    --security-groups $ENDPOINT_SERVER_SEC_GRP)
+
+PRIMARY_ENDPOINT_SERVER_INSTANCE_ID=$(echo $RUN_PRIMARY_ENDPOINT_SERVER | jq -r '.Instances[0].InstanceId')
+
+aws ec2 wait instance-running --instance-ids $PRIMARY_ENDPOINT_SERVER_INSTANCE_ID
+
+PRIMARY_ENDPOINT_SERVER_IP=$(aws ec2 describe-instances  --instance-ids $PRIMARY_ENDPOINT_SERVER_INSTANCE_ID |
+    jq -r '.Reservations[0].Instances[0].PublicIpAddress'
+)
+
+echo "primary endpoint server $PRIMARY_ENDPOINT_SERVER_INSTANCE_ID @ $PRIMARY_ENDPOINT_SERVER_IP"
+
+echo "deploying code to primary endpoint server"
+scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" endpoint.py ubuntu@$PRIMARY_ENDPOINT_SERVER_IP:/home/ubuntu/
+
+echo "setup primary endpoint server..."
+ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PRIMARY_ENDPOINT_SERVER_IP <<EOF
     sudo apt-get update
-    sudo apt-get install redis
-    python3 app.py 34.201.134.71 primary
+    sudo apt-get install python3-flask -y
+    yes Y | sudo apt-get install python3-pip
+    pip3 install redis
+    pip3 install apscheduler
+    pip3 install boto3
+    nohup python3 endpoint.py $REDIS_SERVER_IP primary &>/dev/null &
     exit
 EOF
 
-ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
-    curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
-    sudo apt-get update
-    sudo apt-get install redis
-    python3 app.py 34.201.134.71 secondary
+echo "creating secondary endpoint server..."
+RUN_SECONDARY_ENDPOINT_SERVER=$(aws ec2 run-instances   \
+    --image-id $UBUNTU_20_04_AMI        \
+    --instance-type t3.micro            \
+    --key-name $KEY_NAME                \
+    --security-groups $ENDPOINT_SERVER_SEC_GRP)
+
+SECONDARY_ENDPOINT_SERVER_INSTANCE_ID=$(echo $RUN_SECONDARY_ENDPOINT_SERVER | jq -r '.Instances[0].InstanceId')
+
+echo "waiting for secondary endpoint server creation..."
+aws ec2 wait instance-running --instance-ids $SECONDARY_ENDPOINT_SERVER_INSTANCE_ID
+
+SECONDARY_ENDPOINT_SERVER_IP=$(aws ec2 describe-instances  --instance-ids $SECONDARY_ENDPOINT_SERVER_INSTANCE_ID |
+    jq -r '.Reservations[0].Instances[0].PublicIpAddress'
+)
+
+echo "secondary endpoint server $SECONDARY_ENDPOINT_SERVER_INSTANCE_ID @ $SECONDARY_ENDPOINT_SERVER_IP"
+
+echo "deploying code to secondary endpoint server"
+scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" endpoint.py ubuntu@$SECONDARY_ENDPOINT_SERVER_IP:/home/ubuntu/
+
+echo "setup secondary endpoint server..."
+ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$SECONDARY_ENDPOINT_SERVER_IP <<EOF
+    sudo apt update
+    sudo apt install python3-flask -y
+    python3 endpoint.py $REDIS_SERVER_IP secondary
     exit
 EOF
-
-# Setup load balancer
--- HW2/endpoint.py
+++ HW2/endpoint.py
@@ -20,11 +20,13 @@ def get_redis_conn(redis_server_ip):
 
 @app.route('/enqueue', methods=['PUT'])
 def execute_enqueue():
+    work_id = str(uuid.uuid1())
     iterations_arg = request.args.get('iterations')
     iterations = int(re.findall('\d+', iterations_arg)[0])
-    work_id = str(uuid.uuid1())
+    # TODO: generate payload from request, piazza post: https://piazza.com/class/l051k4nh7g44pf?cid=31
+    payload = 'payload'
     redis_conn = get_redis_conn(app.config.get('redis_server_ip'))
-    redis_conn.rpush(WORK_QUEUE, json.dumps({'work_id': work_id, 'iterations': iterations}))
+    redis_conn.rpush(WORK_QUEUE, json.dumps({'work_id': work_id, 'iterations': iterations, 'payload': payload}))
     return Response(mimetype='application/json',
                     response=json.dumps({'work_id': work_id}),
                     status=200)
@@ -43,7 +45,7 @@ def execute_pull_completed():
 
 def report_work_queue_len(redis_server_ip):
     redis_conn = get_redis_conn(redis_server_ip)
-    cloud_watch = boto3.client('cloudwatch')
+    cloud_watch = boto3.client('cloudwatch', region_name='us-east-1')
     cloud_watch.put_metric_data(
         MetricData=[
             {
@@ -61,7 +63,7 @@ if __name__ == '__main__':
     is_primary = sys.argv[2] == 'primary'
     if is_primary:
         scheduler = BackgroundScheduler()
-        scheduler.add_job(func=report_work_queue_len, args=[redis_server_ip], trigger="interval", seconds=10)
+        scheduler.add_job(func=report_work_queue_len, args=[redis_server_ip], trigger="interval", seconds=60)
         scheduler.start()
     app.config['redis_server_ip'] = redis_server_ip
-    app.run()
+    app.run(host="0.0.0.0")
